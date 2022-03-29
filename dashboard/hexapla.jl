8731f8965a27751a8f73584bdfcdf3dfda38e9ed
Neel Smith <dnsmith.neel@gmail.com>
2022-03-29 16:59:22 -0400

    Add data

--- dashboard/hexapla.jl
+++ dashboard/hexapla.jl
@@ -14,10 +14,31 @@ DASHBOARD_VERSION = "0.1.0"
 # on the assumption that the dashboard will be started
 # from the root of the gh repository!
 assets = joinpath(pwd(), "dashboard", "assets")
+datadir = joinpath(pwd(),"data")
 DEFAULT_PORT = 8050
 
 using Dash
 
+function loadtexts(dir)
+    txts = filter(f -> endswith(f, ".txt"), readdir(dir))
+    langcodes = map(t -> t[1:3], txts)
+    metadatalines = readlines(joinpath(dir, "sources.cex"))
+    metadata = map(ln -> split(ln, "|"), metadatalines[2:end])
+    mddict = Dict()
+    for cols in metadata
+        mddict[cols[4]] = cols[1]
+    end
+    (mddict, txts, langcodes)
+end
+(titlesdict, filenames, langs)  = loadtexts(datadir)
+
+function msoptions(files, titles)
+    opts = []
+    for f in files
+        push!(opts, (label = titles[f], value = f))
+    end
+    opts
+end
 
 app = if haskey(ENV, "URLBASE")
     dash(assets_folder = assets, url_base_pathname = ENV["URLBASE"])
@@ -33,6 +54,11 @@ app.layout = html_div(className = "w3-container") do
     html_h1() do 
         dcc_markdown("Hexapla text reader")
     end,
+    dcc_dropdown(
+        id="translations",
+        options = msoptions(filenames, titlesdict),
+        value = "MTL",
+    ),
 
     html_div(className="w3-container",
         html_div(className="w3-col l6 m6",
